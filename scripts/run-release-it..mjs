100ea2885845bf7a2dcb686898d71ab1156cd66b
Martin Donadieu <martindonadieu@gmail.com>
2024-06-28 04:51:06 +0100

    fix: use mjs

--- scripts/run-release-it..mjs
+++ scripts/run-release-it..mjs
@@ -1,6 +1,9 @@
-// scripts/run-release-it.js
-const { execSync } = require('child_process');
-const path = require('path');
+import { execSync } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const packageName = process.argv[2];
 if (!packageName) {
